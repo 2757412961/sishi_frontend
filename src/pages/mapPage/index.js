c088ef1ed5ebf877c6a0c72846089f9766710174
Gillian1998457 <2376296725@qq.com>
2021-03-09 21:29:56 +0800

    加了model,把mapPage改为Class

--- src/pages/mapPage/index.js
+++ src/pages/mapPage/index.js
@@ -15,51 +15,51 @@ const Authorized = RenderAuthorized(getAuthority());
 const { Countdown } = Statistic;
 const { Content, Sider } = Layout;
 const noMatch=<Redirect to={`/login?redirect=${window.location.href}`} />;
- // Moment is also OK
-function MapPage(props) {
-  const [_collapsed, setCollapsed] = useState(false);
-  const [modalVisble,setModalVisble]=useState(false);
-  const [deadline,setDeadline] =useState( Date.now() +  1000 * 60);
-
-
+class MapPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _collapsed: false,
+      modalVisble: false,
+      deadline: Date.now() +  1000 * 60,
+    };
+  }
+  render(){
   return (
     <Authorized authority={['NORMAL','admin']} noMatch={noMatch}>
     <Layout className={styles.normal}>
       <Sider style={{backgroundColor:'white'}} width={300}>
-        <Button onClick={() =>setModalVisble(true)}>开始答题</Button>
-        <Modal visible={modalVisble}
+        <Button onClick={() =>this.setState({modalVisble:true})}>开始答题</Button>
+        <Modal visible={this.state.modalVisble}
                title="开始答题"
                centered
-               //width={'60vw'}
-               // okText="确认" cancelText="取消"
-               // onOk={() => {setModalVisble(false)}}
-               onCancel={() => {}}
                style={{top:'3em'}}
                bodyStyle={{height:'70vh'}}
                maskStyle={{backgroundColor: 'rgba(198,170,145,1)' ,top:'5em',}}
                footer={[
                  <Row gutter={16}>
                    <Col span={8}>
-                 <Button  key="back" onClick={()=>{}}>
-                 上一题</Button>
+                     <Button  key="back" onClick={()=>{}}>
+                       上一题
+                     </Button>
                    </Col>
                    <Col span={8}>
-                 <Button
-                   key="submit"
-                   type="primary"
-                   onClick={()=> {
-                     setModalVisble(false)
-                     setDeadline(Date.now() +  1000 * 60)
-                   }}
-                 >
-                   下一题
-                 </Button></Col>
-                   <Col span={8}>
-                 <Countdown center style={{right:'26em'}} title="Countdown" value={deadline} onFinish={()=>{}} />
+                     <Button
+                       key="submit"
+                       type="primary"
+                       onClick={()=> {
+                         this.setState({modalVisble:false})
+                         this.setState({deadline:Date.now() +  1000 * 60})
+                       }}>
+                       下一题
+                     </Button>
                    </Col>
-                   </Row>
+                   <Col span={8}>
+                     <Countdown title="Countdown" value={this.state.deadline} onFinish={()=>{}} />
+                   </Col>
+                 </Row>
                  ]}
-        >{deadline}
+        >{this.state.deadline}
           <p>Some contents...</p>
           <p>Some contents...</p>
           <p>Some contents...</p>
@@ -70,9 +70,62 @@ function MapPage(props) {
       </Content>
     </Layout>
     </Authorized>
-  );
+  );}
 }
 
-export default connect(({  }) => ({
-
+// function MapPage(props) {
+//   const [_collapsed, setCollapsed] = useState(false);
+//   const [modalVisble,setModalVisble]=useState(false);
+//   const [deadline,setDeadline] =useState( Date.now() +  1000 * 60);
+//
+//
+//   return (
+//     <Authorized authority={['NORMAL','admin']} noMatch={noMatch}>
+//       <Layout className={styles.normal}>
+//         <Sider style={{backgroundColor:'white'}} width={300}>
+//           <Button onClick={() =>setModalVisble(true)}>开始答题</Button>
+//           <Modal visible={modalVisble}
+//                  title="开始答题"
+//                  centered
+//                  style={{top:'3em'}}
+//                  bodyStyle={{height:'70vh'}}
+//                  maskStyle={{backgroundColor: 'rgba(198,170,145,1)' ,top:'5em',}}
+//                  footer={[
+//                    <Row gutter={16}>
+//                      <Col span={8}>
+//                        <Button  key="back" onClick={()=>{}}>
+//                          上一题
+//                        </Button>
+//                      </Col>
+//                      <Col span={8}>
+//                        <Button
+//                          key="submit"
+//                          type="primary"
+//                          onClick={()=> {
+//                            setModalVisble(false)
+//                            setDeadline(Date.now() +  1000 * 60)
+//                          }}>
+//                          下一题
+//                        </Button>
+//                      </Col>
+//                      <Col span={8}>
+//                        <Countdown title="Countdown" value={deadline} onFinish={()=>{}} />
+//                      </Col>
+//                    </Row>
+//                  ]}
+//           >{deadline}
+//             <p>Some contents...</p>
+//             <p>Some contents...</p>
+//             <p>Some contents...</p>
+//           </Modal>
+//         </Sider>
+//         <Content>
+//           <MapPageMap/>
+//         </Content>
+//       </Layout>
+//     </Authorized>
+//   );
+// }
+export default connect(({ mapPage }) => ({
+mapPage
 }))(MapPage);
