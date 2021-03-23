76861598def6571a6e21aedf4b382c1baf23e4f9
Gillian1998457 <2376296725@qq.com>
2021-03-23 10:52:04 +0800

    改了图片视频文章的样式,改了中共一大无法显示的bug

--- src/pages/mapPage/index.js
+++ src/pages/mapPage/index.js
@@ -53,7 +53,7 @@ const variants={open: { opacity: 1, x: 0 },
   closed: { opacity: 0, x: "-100%" },}
 //
 //
-// const list = [
+// const list =
 //   {
 //     id:'一大-上海',
 //     lonlat:[121.47069346816863, 31.22206084685108],
@@ -331,11 +331,9 @@ function forList(treeList){
       temp.lonlat=treeList[i].geoCoordinates;
       temp.tagName=treeList[i].tagName;
       temp.text=treeList[i].label;
-      temp.value=treeList[i].label;
+      temp.value=treeList[i].label.replace('@','');
       temp.time=treeList[i].time;
-      temp.showInfo='<div className={styles.markerTop}><h2>'+treeList[i].label+'</h2></div> <div className={styles.markerBody}><p>中国共产党第一次全国代表大会，简称中共一大，' +
-        '于'+treeList[i].time+'在<span>'+treeList[i].label+'</span>法租界秘密召开，7月30日会场被租界巡捕房搜查后休会，8月3日在浙江省<span>嘉兴</span>闭幕结束。' +
-        '大会的召开宣告了中国共产党的正式成立。</p> <p><a id="btn">点击进入学习卡片</a></p></div>';
+      temp.showInfo='<div className={styles.markerTop}><h2>'+treeList[i].label+'</h2></div>';
       temp.cardContent=treeList[i].tagName;
       temp.cardImg=p1;
       list.push(temp);
@@ -387,6 +385,7 @@ class MapPage extends Component {
       startPicture:false,
       startVideo:false,
       startAudio:false,
+      tagName:'',
     };
 
   }
@@ -435,9 +434,38 @@ class MapPage extends Component {
       console.log('res',res);
       if(res&&res.success){
         let tagTree=res.list;
-        // let tree=forTree(tagTree);
-        // console.log('tree',tree);
         list=forList(tagTree);
+      }
+      let _this = this;
+      for(let i = 0;i<list.length;i++){
+        map.on('click', list[i].id, function(e) {
+          var coordinates = e.features[0].geometry.coordinates;
+          _this.setState({
+            itemNow: list[i],
+            tagName:list[i].tagName,
+          })
+          new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            // .setHTML(showInfo)
+            .addTo(map)
+            .setDOMContent(popupRef.current);
+          // document.getElementById('btn')
+          //   .addEventListener('click', function(){
+          //     let cardImg = list[i].cardImg;
+          //     let cardContent = list[i].cardContent;
+          //     _this.setState({
+          //       knowledgeUrl: cardImg,
+          //       knowledgeContent: cardContent,
+          //     });
+          //     _this.showModal()
+          //   });
+        });
+        map.on('mouseenter', list[i].id, function() {
+          map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', list[i].id, function() {
+          map.getCanvas().style.cursor = '';
+        });
       }
     });
     /*const map = new Scene({
@@ -727,38 +755,6 @@ class MapPage extends Component {
       }
       // playback(0);
     });
-    let _this = this;
-    for(let i = 0;i<list.length;i++){
-      map.on('click', list[i].id, function(e) {
-        var coordinates = e.features[0].geometry.coordinates;
-        let showInfo = list[i].showInfo;
-        _this.setState({
-          itemNow: list[i],
-        })
-
-        new mapboxgl.Popup()
-          .setLngLat(coordinates)
-          // .setHTML(showInfo)
-          .addTo(map)
-          .setDOMContent(popupRef.current);
-        // document.getElementById('btn')
-        //   .addEventListener('click', function(){
-        //     let cardImg = list[i].cardImg;
-        //     let cardContent = list[i].cardContent;
-        //     _this.setState({
-        //       knowledgeUrl: cardImg,
-        //       knowledgeContent: cardContent,
-        //     });
-        //     _this.showModal()
-        //   });
-      });
-      map.on('mouseenter', list[i].id, function() {
-        map.getCanvas().style.cursor = 'pointer';
-      });
-      map.on('mouseleave', list[i].id, function() {
-        map.getCanvas().style.cursor = '';
-      });
-    }
     const myDeckLayer = new MapboxLayer({
       id: 'arc',
       type: ArcLayer,
@@ -821,68 +817,20 @@ class MapPage extends Component {
   };
 
   render(){
-    let question1='中日甲午战争中，日军野蛮屠杀和平居民的地点是';
-    let answer=['A.大连','B.旅顺','C.平壤','D.花园口'];
-    let rightAnswer=1;
-    // const {dispatch}=this.props;
-    // dispatch({ type: 'mapPage/getTagTree'});
-    // dispatch({ type: 'mapPage/getQuestion'});
-    // console.log('dispatch',dispatch);
     const {mapPage}=this.props;
     console.log('mapPage',mapPage);
-    //debugger
-    // let tree=[];
-    // function forTree1(treeList){
-    //   for (let i in treeList){
-    //     console.log('i',i);
-    //     if(treeList[i].children.length>0){
-    //       forTree(treeList[i].children)
-    //     }else{
-    //       tree.push(treeList[i])
-    //     }
-    //   }
-    //   return tree
-    // }
-    tree=[];
     const {tagTree,question}=mapPage;
-    // let list1=forTree(tagTree);
     list=forList(tagTree);
     console.log('listRender',list);
     let allNumber=question.length;
     let recent=this.state.questionNumber-1
     console.log('tagTree',tagTree);
+    console.log('tagName',this.state.tagName);
     //遍历tagTree;
-    let tree=[];
-    function forTree(treeList){
-      for (let i in treeList){
-        console.log('i',i);
-        if(treeList[i].children.length>0){
-          forTree(treeList[i].children)
-        }else{
-          tree.push(treeList[i])
-        }
-      }
-      return tree
-    }
-    //遍历树生成的数组treeList
-    // let treeList=forTree(tagTree);
-    const {unCheckStyle,checkStyle} = this.state;
   return (
     <Authorized authority={['NORMAL','admin']} noMatch={noMatch}>
     <Layout className={styles.normal}>
       <Sider style={{backgroundColor:'rgba(155,100,20,0.5)', overflow:'auto'}} width={400}>
-        <Button  key="back" onClick={()=>{this.setState({startQuestion:true})}}>
-          答题
-        </Button>
-        <Button  key="back" onClick={()=>{this.setState({startArticle:true})}}>
-          文章
-        </Button>
-        <Button  key="back" onClick={()=>{this.setState({startPicture:true})}}>
-          图片
-        </Button>
-        <Button  key="back" onClick={()=>{this.setState({startVideo:true})}}>
-          视频
-        </Button>
         {/*答题*/}
         <Modal visible={this.state.startQuestion}
                centered
@@ -1071,10 +1019,6 @@ class MapPage extends Component {
             <div className={styles.topVideo}></div>
             <video height="400" width="100%" top="3em" poster="http://www.youname.com/images/first.png" autoPlay="autoplay" preload="none"
                    controls="controls">
-              {/*<source src="./1.mp4"*/}
-              {/*/>*/}
-              {/*<source src="./1.mp4"*/}
-              {/*/>*/}
               <source src="http://192.168.2.2:89/media/videos/dangshi/05.mp4"
               />
               <source src="http://192.168.2.2:89/media/videos/dangshi/05.mp4"
@@ -1146,28 +1090,28 @@ class MapPage extends Component {
             <div  ref={popupRef} className={styles.popupDiv}>
               {/*<div style={{margin:"0 auto", color:"red", fontSize:"20px", textAlign:"center"}}>{this.state.itemNow['id']}</div>*/}
               <Row style={{width:"240px",top:"10px"}} justify="space-between">
-                <Col span={2} onClick={()=>this.showModal("1")}>
+                <Col span={2} onClick={()=>{this.setState({startArticle:true})}}>
                   <Icon className={styles.popup} type="book" />
                 </Col>
-                <Col span={4} onClick={()=>this.showModal("1")}>
+                <Col span={4} onClick={()=>{this.setState({startArticle:true})}}>
                   文章
                 </Col>
-                <Col span={2} onClick={()=>this.showModal("2")}>
+                <Col span={2} onClick={()=>{this.setState({startPicture:true})}}>
                   <Icon className={styles.popup} type="picture" />
                 </Col>
-                <Col span={4} onClick={()=>this.showModal("2")}>
+                <Col span={4} onClick={()=>{this.setState({startPicture:true})}}>
                   图片
                 </Col>
-                <Col span={2} onClick={()=>this.showModal("3")}>
+                <Col span={2} onClick={()=>{this.setState({startVideo:true})}}>
                   <Icon className={styles.popup} type="video-camera" />
                 </Col>
-                <Col span={4} onClick={()=>this.showModal("3")}>
+                <Col span={4} onClick={()=>{this.setState({startVideo:true})}}>
                   视频
                 </Col>
-                <Col span={2} onClick={()=>this.showModal("4")}>
+                <Col span={2} onClick={()=>this.setState({startQuestion:true})}>
                   <Icon className={styles.popup} type="question" />
                 </Col>
-                <Col span={4} onClick={()=>this.showModal("4")}>
+                <Col span={4} onClick={()=>this.setState({startQuestion:true})}>
                   答题
                 </Col>
               </Row>
